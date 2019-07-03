5c01eb3899b2b3f3c625bc0de05204281b546787
Alex <infragreen22@gmail.com>
2019-07-03 09:14:38 +0100

    A blueprint of the UserViewModel

--- app/src/main/java/acr18as/sheffield/ac/uk/takemeback/viewmodel/UserViewModel.java
+++ app/src/main/java/acr18as/sheffield/ac/uk/takemeback/viewmodel/UserViewModel.java
@@ -1,6 +1,31 @@
 package acr18as.sheffield.ac.uk.takemeback.viewmodel;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class UserViewModel extends ViewModel {
+import acr18as.sheffield.ac.uk.takemeback.UserClient;
+import acr18as.sheffield.ac.uk.takemeback.model.User;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class UserViewModel extends AndroidViewModel {
+
+    private MutableLiveData<User> user;
+
+    public UserViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<User> getUser() {
+        if (user == null) {
+            user = new MutableLiveData<User>();
+            loadUser();
+        }
+        return user;
+    }
+
+    private void loadUser() {
+        //user = ((UserClient)getApplication().getApplicationContext()).getUser();
+    }
 }
